e1571b31cd1e8b692647cc96e38a47125760de81
Mariya Kaverina <kaverina.ma@mail.ru>
2024-01-06 19:39:54 +0400

    расширенная версия задачи

---
+++ exampl/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+    Console.Write("Первое число" + "`" + firstNumber + "`" + " больше чем второе число " + "`" + secondNumber + "`");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.Write("Первое число " + "`" + firstNumber + "`" + " меньше чем второе число " + "`" + secondNumber + "`");
+}
+else
+{
+    Console.Write("Введенные числа равны " + "`" + firstNumber + "`");
+}
+
+
