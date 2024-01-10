bc9080734d8045d50c12465a9aa593551f48e907
Mariya Kaverina <kaverina.ma@mail.ru>
2024-01-10 19:54:09 +0400

    add Lesson1:Task1

--- exampl/Task1/Program.cs
+++ exampl/Task1/Program.cs
@@ -1,18 +1,45 @@
-﻿Console.WriteLine("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-if (firstNumber > secondNumber)
+﻿// Console.WriteLine("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber < secondNumber)
+// {
+//     Console.Write("второе число больше");
+// }
+// else if (firstNumber > secondNumber)
+// {
+//     Console.Write("первое число больше");
+// }
+// else
+// {
+//     Console.Write("числа равны ");
+// }
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
 {
-    Console.Write("Первое число" + "`" + firstNumber + "`" + " больше чем второе число " + "`" + secondNumber + "`");
-}
-else if (firstNumber < secondNumber)
-{
-    Console.Write("Первое число " + "`" + firstNumber + "`" + " меньше чем второе число " + "`" + secondNumber + "`");
+    Console.WriteLine(N);
 }
 else
 {
-    Console.Write("Введенные числа равны " + "`" + firstNumber + "`");
+    string PrintString = "";
+    string CurrentString = " ";
+    while (N > 0)
+    {
+        int currentDigit = N % 10;
+        N /= 10;
+        CurrentString = currentDigit.ToString();
+        if (N > 0)
+        {
+            PrintString = ("," + CurrentString  + PrintString);
+        }
+        else
+        {
+           
+           PrintString = (CurrentString  + PrintString);
+            Console.WriteLine(PrintString);
+        }
+    }
+   // Console.WriteLine(printString);
 }
-
-
---
+++ exampl/Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿// int max = 0;
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+// if(firstNumber > max)
+// {
+//     max = firstNumber;
+// }
+// if(secondNumber > max)
+// {
+//     max = secondNumber;
+// }
+// if(thirdNumber > max)
+// {
+//     max = thirdNumber;
+// }
+
+// Console.WriteLine("max = " + max);
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int a = number % 2;
+
+// if(a == 0)
+// {
+//     Console.WriteLine("четное");
+// }
+// else
+// {
+//     Console.WriteLine("нечетное");
+// }
+
+
+//int max = 0;
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber % 7 == 0 && firstNumber % 23 == 0)
+// {
+//     Console.WriteLine("делится");
+// }
+// else
+// {
+//     Console.WriteLine("не делится");
+// }
+
+Console.Write("Введите координату x, не равную 0: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y, не равную 0: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Первая четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
+else if (x == 0 && y == 0)
+{
+    Console.WriteLine("Начало координат");
+}
+else 
+{
+    Console.WriteLine("Одна из координат = 0");
+}
